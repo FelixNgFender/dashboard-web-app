2e1a4e6e1b250758012ac04fe7671436776f151b
Thinh Nguyen <ngthinh302@gmail.com>
2022-11-28 23:23:12 -0500

    Finish EDA and retrain model

--- pages/eda.py
+++ pages/eda.py
@@ -3,6 +3,7 @@ from dash import dcc, html, callback, Output, Input
 import plotly.express as px
 import pandas as pd
 import dash_bootstrap_components as dbc
+import re
 
 dash.register_page(__name__, name="Exploratory Data Analysis")
 
@@ -26,32 +27,43 @@ df.drop('max', axis=1, inplace=True)
 # Compute the correlation matrix
 corr = df.corr()
 
+# Pre-failure signal plotting
+def plot_signal(unit_id, signal_name, window_size):
+    data = df[df['id'] == unit_id].rolling(window_size).mean()
+    fig = px.line(data, x='RUL', y=signal_name, title=str(sensor_dict[signal_name]) + " before failure on unit " + str(unit_id))
+    fig['layout']['xaxis']['autorange'] = "reversed"
+    return fig
+
+sensor_dict = {'s1': 'Fan inlet temperature (◦R)',
+            's2': 'LPC outlet temperature (◦R)',
+            's3': 'HPC outlet temperature (◦R)',
+            's4': 'LPT outlet temperature (◦R)',
+            's5': 'Fan inlet Pressure (psia)',
+            's6': 'Bypass-duct pressure (psia)',
+            's7': 'HPC outlet pressure (psia)',
+            's8': 'Physical fan speed (rpm)',
+            's9': 'Physical core speed (rpm)',
+            's10': 'Engine pressure ratio (P50/P2)',
+            's11': 'HPC outlet Static pressure (psia)',
+            's12': 'Ratio of fuel flow to Ps30 (pps/psia)',
+            's13': 'Corrected fan speed (rpm)',
+            's14': 'Corrected core speed (rpm)',
+            's15': 'Bypass Ratio',
+            's16': 'Burner fuel-air ratio',
+            's17': 'Bleed Enthalpy',
+            's18': 'Required fan speed',
+            's19': 'Required fan conversion speed',
+            's20': 'High-pressure turbines Cool air flow',
+            's21': 'Low-pressure turbines Cool air flow'}
+
 # Build components
-myDropdown1 = dcc.Dropdown(options=['Max cycle lifetime for all units', 'Distribution of maximum lifetime in cycles','Correlation matrix'],
-                        value='Max cycle lifetime for all units')
+myDropdown1 = dcc.Dropdown(options=['Max cycle lifetime for all units', 'Distribution of maximum lifetime in cycles',
+                                    'Correlation matrix', 'Moving average of sensor data before failure',
+                                    'Distribution of sensor data for all units'],
+                            value='Max cycle lifetime for all units')
 myDropdown2 = dcc.Dropdown(options=[''.join(['Unit ', str(i)]) for i in range(1, 101)],
-                        value='Unit 1')
-myDropdown3 = dcc.Dropdown(options=['s1: Fan inlet temperature (◦R)',
-                                    's2: LPC outlet temperature (◦R)',
-                                    's3: HPC outlet temperature (◦R)',
-                                    's4: LPT outlet temperature (◦R)',
-                                    's5: Fan inlet Pressure (psia)',
-                                    's6: bypass-duct pressure (psia)',
-                                    's7: HPC outlet pressure (psia)',
-                                    's8: Physical fan speed (rpm)',
-                                    's9: Physical core speed (rpm)',
-                                    's10: Engine pressure ratio (P50/P2)',
-                                    's11: HPC outlet Static pressure (psia)',
-                                    's12: Ratio of fuel flow to Ps30 (pps/psia)',
-                                    's13: Corrected fan speed (rpm)',
-                                    's14: Corrected core speed (rpm)',
-                                    's15: Bypass Ratio',
-                                    's16: Burner fuel-air ratio',
-                                    's17: Bleed Enthalpy',
-                                    's18: Required fan speed',
-                                    's19: Required fan conversion speed',
-                                    's20: High-pressure turbines Cool air flow',
-                                    's21: Low-pressure turbines Cool air flow'],
+                            value='Unit 1')
+myDropdown3 = dcc.Dropdown(options=[str(key) + ": " + str(value) for key, value in sensor_dict.items()],
                             value='s1: Fan inlet temperature (◦R)')
 myGraph = dcc.Graph()
 
@@ -97,4 +109,8 @@ def updateGraph(value1, value2, value3):
     elif value1 == 'Correlation matrix':
         fig = px.imshow(corr, text_auto=True, height=1600, title='Correlation matrix')
         fig.update_xaxes(side="top")
+    elif value1 == 'Moving average of sensor data before failure':
+        fig = plot_signal(int(value2[5:]), value3.split(':')[0], 10)
+    elif value1 == 'Distribution of sensor data for all units':
+        fig = px.box(df[value3.split(':')[0]], title=value3.split(':')[0])
     return fig
